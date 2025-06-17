0981029fe50ef80202fa969400ffb8f8659174a8
KubaTaba1uga <luna7337anul@proton.me>
2025-06-17 11:53:06 +0200

    finally generic smart pointers are working

--- src/utils/smart_ptr.h
+++ src/utils/smart_ptr.h
@@ -10,21 +10,19 @@
 /* #define SP_GET_VALUE(sp, name) (sp).get->name */
 #include <stdbool.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
 
-#define SP_UNWRAP(sp) (*(sp).get)
-#define SP_GET_PTR(sp, name) &(*(sp).get)->name
-#define SP_SET_VALUE(sp, name, value) (*(sp).get)->name = value
-struct __sp_handlers {
+struct __sp_payload {
   void *value;
   void (*value_destroyh)(void *);
-  bool is_destroyed;
+  void *sp;
 };
 
 static inline struct my_generic_sp *
-sp_get_from_handlers(struct __sp_handlers *handlers);
+sp_get_from_handlers(struct __sp_payload *handlers);
 
-static inline void __sp_handlers_destroy(struct __sp_handlers *sp_handler) {
+static inline void __sp_payload_destroy(struct __sp_payload *sp_handler) {
   if (!sp_handler) {
     return;
   }
@@ -32,68 +30,58 @@ static inline void __sp_handlers_destroy(struct __sp_handlers *sp_handler) {
   if (sp_handler->value && sp_handler->value_destroyh) {
     sp_handler->value_destroyh(sp_handler->value);
   }
-
-  sp_handler->is_destroyed = true;
-
-  void *sp = sp_get_from_handlers(sp_handler);
-
-  /* free(sp); */
 };
 
-static inline struct __sp_handlers
-__sp_handlers_clone(struct __sp_handlers sp) {
-  return (struct __sp_handlers){.value = sp.value,
-                                .value_destroyh = sp.value_destroyh};
+static inline struct __sp_payload __sp_payload_clone(struct __sp_payload sp) {
+  return (struct __sp_payload){.value = sp.value,
+                               .value_destroyh = sp.value_destroyh};
 };
 
 #define i_type my_generic_sp
-#define i_key struct __sp_handlers
-#define i_keydrop __sp_handlers_destroy
-#define i_keyclone __sp_handlers_clone
+#define i_key struct __sp_payload
+#define i_keydrop __sp_payload_destroy
+#define i_keyclone __sp_payload_clone
 #include "stc/arc.h"
 
 static inline void *sp_alloc(uint32_t size, void (*value_destroyh)(void *)) {
   /*
     We are creating our custom smart pointer frame which looks sth like this:
-     |                   |                      |       |
-     | STC smart poniter | struct __sp_handlers | value |
-     |                   |                      |       |
+     |                   |        |                     |       |
+     | STC smart poniter |   ->   | struct __sp_payload | value |
+     |                   |        |                     |       |
 
     Now we can use ptr to `value` part and go back to smart pointer part
     whenever needed.
   */
-  struct my_generic_sp *sp = malloc(sizeof(struct my_generic_sp) +
-                                    sizeof(struct __sp_handlers) + size);
+  struct my_generic_sp *sp = malloc(sizeof(struct my_generic_sp));
   if (!sp) {
     return NULL;
   }
 
-  struct __sp_handlers *sp_handlers =
-      (struct __sp_handlers *)((unsigned char *)sp +
-                               sizeof(struct my_generic_sp));
+  struct __sp_payload *sp_payload = malloc(sizeof(struct __sp_payload) + size);
+  if (!sp_payload) {
+    free(sp);
+    return NULL;
+  }
 
-  void *sp_value = (void *)((unsigned char *)sp + sizeof(struct my_generic_sp) +
-                            sizeof(struct __sp_handlers));
+  void *sp_value =
+      (void *)((unsigned char *)sp_payload + sizeof(struct __sp_payload));
 
-  *sp_handlers = (struct __sp_handlers){.value = sp_value,
-                                        .value_destroyh = value_destroyh,
-                                        .is_destroyed = false};
+  *sp_payload = (struct __sp_payload){
+      .value = sp_value,
+      .value_destroyh = value_destroyh,
+      .sp = sp,
+  };
 
-  *sp = my_generic_sp_from_ptr(sp_handlers);
+  *sp = my_generic_sp_from_ptr(sp_payload);
 
   return sp_value;
 };
 
 static inline struct my_generic_sp *sp_get(void *value) {
-  return (struct my_generic_sp *)((unsigned char *)value -
-                                  sizeof(struct my_generic_sp) -
-                                  sizeof(struct __sp_handlers));
-};
-
-static inline struct my_generic_sp *
-sp_get_from_handlers(struct __sp_handlers *handlers) {
-  unsigned char *data = (unsigned char *)handlers;
-  return (struct my_generic_sp *)(data - sizeof(struct my_generic_sp));
+  return ((struct __sp_payload *)((unsigned char *)value -
+                                  sizeof(struct __sp_payload)))
+      ->sp;
 };
 
 static inline void *sp_ref(void *value) {
@@ -106,10 +94,10 @@ static inline void *sp_ref(void *value) {
 
 static inline void sp_deref(void *value) {
   struct my_generic_sp *sp = sp_get(value);
-
+  uint32_t use_count = my_generic_sp_use_count(sp);
   my_generic_sp_drop(sp);
 
-  if (sp->get->is_destroyed) {
+  if (--use_count == 0) {
     free(sp);
   }
 }
